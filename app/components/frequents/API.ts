c6ebd7d0a5c0780ac814013b24c258b0adf1f39e
mehdimoosavi7 <mehdimoosavi7@gmail.com>
2024-05-28 17:09:20 +0330

    heroku uninstall

--- app/components/frequents/API.ts
+++ app/components/frequents/API.ts
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:3000/api/";
-export const API_URL = "https://cryptic-headland-94862.herokuapp.com/https://samplestore.hsng.vercel.app/api/";
+export const API_URL = "https://samplestore.hsng.vercel.app/api/";
