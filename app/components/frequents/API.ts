91c1736fa64eb3ab1376dce9e133bbad376ede40
mehdimoosavi7 <mehdimoosavi7@gmail.com>
2024-05-28 11:50:42 +0330

    change api address

--- app/components/frequents/API.ts
+++ app/components/frequents/API.ts
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:3000/api/";
-export const API_URL = "https://samplestore-hnsg.vercel.app/api/";
+export const API_URL = "https://samplestore.vercel.app/api/";
